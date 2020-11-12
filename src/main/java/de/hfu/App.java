55f886d61dfdbdefae53af36e39ee83fcb14c0b0
Louis Kuhnt <louis.ronald.kuhnt@hs-furtwangen.de>
2020-11-12 14:46:08 +0100

    added some comments

--- src/main/java/de/hfu/App.java
+++ src/main/java/de/hfu/App.java
@@ -11,12 +11,16 @@ public class App
     public static void main( String[] args )
     {
         while(true) {
+        	//Erstellt Scanner, um die Konsole auszulesen
             Scanner scanner = new Scanner(System.in);
             System.out.println("Hello World!");
             System.out.println("Eingabe eines Wortes!");
+            //Holt Daten aus der Konsole und schreibt Sie in eine Variable
             String eingabe = scanner.nextLine();
 
+            //Durchläuft den String um jeden Charakter
             for (char c : eingabe.toCharArray()) {
+            	//Wandelt um in Grossbuchstaben, wenn nötig
                 if (Character.isLowerCase(c)) {
                     System.out.println(Character.toUpperCase(c));
                 } else {
